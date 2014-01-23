ce4f4c68fdb77956cd05b24c0da4e3728df8af61
Tareq Hasan <tareq1988@gmail.com>
2014-01-23 17:24:03 +0600

    open-sans font added

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,22 +11,23 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function wedocs_scripts() {
-    wp_enqueue_style('wedocs-main', get_template_directory_uri() . '/assets/css/main.min.css', false );
-    wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css', false );
+    wp_enqueue_style( 'open-sans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' );
+    wp_enqueue_style( 'wedocs-main', get_template_directory_uri() . '/assets/css/main.min.css', false );
+    wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css', false );
 
     if (is_single() && comments_open() && get_option('thread_comments')) {
-        wp_enqueue_script('comment-reply');
+        wp_enqueue_script( 'comment-reply' );
     }
 
-    wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', array(), null, false);
-    wp_register_script('wedocs-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array(), false, true);
-    wp_enqueue_script('modernizr');
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('bootstrap-tooltip', get_template_directory_uri() . '/assets/js/plugins/bootstrap/tooltip.js', array(), false, true);
-    wp_enqueue_script('bootstrap-scrollspy', get_template_directory_uri() . '/assets/js/plugins/bootstrap/scrollspy.js', array(), false, true);
-    wp_enqueue_script('bootstrap-dropdown', get_template_directory_uri() . '/assets/js/plugins/bootstrap/dropdown.js', array(), false, true);
-    wp_enqueue_script('bootstrap-collapse', get_template_directory_uri() . '/assets/js/plugins/bootstrap/collapse.js', array(), false, true);
-    wp_enqueue_script('wedocs-scripts');
+    wp_register_script( 'modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', array(), null, false);
+    wp_register_script( 'wedocs-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array(), false, true);
+    wp_enqueue_script( 'modernizr');
+    wp_enqueue_script( 'jquery');
+    wp_enqueue_script( 'bootstrap-tooltip', get_template_directory_uri() . '/assets/js/plugins/bootstrap/tooltip.js', array(), false, true);
+    wp_enqueue_script( 'bootstrap-scrollspy', get_template_directory_uri() . '/assets/js/plugins/bootstrap/scrollspy.js', array(), false, true);
+    wp_enqueue_script( 'bootstrap-dropdown', get_template_directory_uri() . '/assets/js/plugins/bootstrap/dropdown.js', array(), false, true);
+    wp_enqueue_script( 'bootstrap-collapse', get_template_directory_uri() . '/assets/js/plugins/bootstrap/collapse.js', array(), false, true);
+    wp_enqueue_script( 'wedocs-scripts' );
 }
 
 add_action('wp_enqueue_scripts', 'wedocs_scripts', 100);
