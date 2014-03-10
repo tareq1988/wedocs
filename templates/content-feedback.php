b6488329ec6fc4b5bf38c1d2639324bd6b0a4dcd
Tareq Hasan <tareq1988@gmail.com>
2014-03-10 15:33:18 +0600

    feedback negative print bug fix

--- templates/content-feedback.php
+++ templates/content-feedback.php
@@ -6,7 +6,7 @@
     $negative = (int) get_post_meta( $post->ID, 'negative', true );
 
     $positive_title = $positive ? sprintf( _n( '%d person found this useful', '%d persons found this useful', $positive, 'wedocs' ), $positive ) : __( 'No votes yet', 'wedocs' );
-    $negative_title = $negative ? printf( _n( '%d person found this not useful', '%d persons found this not useful', $negative, 'wedocs' ), $negative ) : __( 'No votes yet', 'wedocs' );
+    $negative_title = $negative ? sprintf( _n( '%d person found this not useful', '%d persons found this not useful', $negative, 'wedocs' ), $negative ) : __( 'No votes yet', 'wedocs' );
     ?>
     <a href="#" class="tip positive" data-id="<?php the_ID(); ?>" data-type="positive" title="<?php echo esc_attr( $positive_title ); ?>"><i class="fa fa-check-square-o"></i> <?php echo $positive; ?></a>
     <a href="#" class="tip negative" data-id="<?php the_ID(); ?>" data-type="negative" title="<?php echo esc_attr( $negative_title ); ?>"><i class="fa fa-times"></i> <?php echo $negative; ?></a>
