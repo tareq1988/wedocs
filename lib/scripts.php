c3d6cc58e95cbb80c33af8e16bcb7cf11ef2dd8a
Tareq Hasan <tareq1988@gmail.com>
2014-09-23 20:26:55 +0600

    stylesheet renamed to app.css

--- lib/scripts.php
+++ lib/scripts.php
@@ -12,7 +12,7 @@
  */
 function wedocs_scripts() {
     wp_enqueue_style( 'open-sans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' );
-    wp_enqueue_style( 'wedocs-main', get_template_directory_uri() . '/assets/css/main.min.css', false );
+    wp_enqueue_style( 'wedocs-main', get_template_directory_uri() . '/assets/css/app.css', false );
     wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css', false );
 
     if (is_single() && comments_open() && get_option('thread_comments')) {
