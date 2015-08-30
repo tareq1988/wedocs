38e85888b2c6f8c97067a47fb8230d9c60da6c37
Tareq Hasan <tareq1988@gmail.com>
2015-08-31 00:47:32 +0600

    Grunt support, separate bootstrap css, sourcemap

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,42 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        // setting folder templates
+        dirs: {
+            css: 'assets/css',
+            less: 'assets/less',
+            fonts: 'assets/fonts',
+            images: 'assets/images',
+            js: 'assets/js'
+        },
+
+        // Compile all .less files.
+        less: {
+
+            // one to one
+            core: {
+                options: {
+                    sourceMap: true,
+                    sourceMapFilename: 'style.css.map',
+                    sourceMapURL: 'style.css.map',
+                    sourceMapRootpath: '../../'
+                },
+                files: {
+                    'style.css': '<%= dirs.less %>/app.less'
+                }
+            },
+        },
+
+        watch: {
+            less: {
+                files: ['<%= dirs.less %>/*.less'],
+                tasks: ['less:core'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+
         // Generate POT files.
         makepot: {
             target: {
@@ -20,4 +56,6 @@ module.exports = function(grunt) {
 
     // Load NPM tasks to be used here
     grunt.loadNpmTasks( 'grunt-wp-i18n' );
+    grunt.loadNpmTasks( 'grunt-contrib-less' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
 };
